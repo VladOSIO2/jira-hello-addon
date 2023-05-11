94374b7e3d349e74a48bd37165648d23c7645ee0
VladoSIO2 <vl4ds1mus@gmail.com>
2023-05-11 20:34:52 +0300

    Make greeting endpoint output entire host user info

--- src/main/java/com/techstack/jirahello/controller/HelloController.java
+++ src/main/java/com/techstack/jirahello/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.techstack.jirahello.controller;
 
+import com.atlassian.connect.spring.AtlassianHostUser;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,10 +11,10 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloController {
 
     @GetMapping("/hello-world")
-    public ModelAndView helloWorld(Authentication authentication) {
+    public ModelAndView helloWorld(@AuthenticationPrincipal AtlassianHostUser hostUser) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("hello");
-        modelAndView.addObject("authentication", authentication);
+        modelAndView.addObject("authentication", String.valueOf(hostUser));
         return modelAndView;
     }
 }
